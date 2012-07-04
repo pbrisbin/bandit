c250930e9db60556f77e3b0fe0f849f796e68c93
patrick brisbin <pbrisbin@gmail.com>
2012-07-03 23:08:47 -0400

    small refactor to main

--- lib/bandit.rb
+++ lib/bandit.rb
@@ -7,31 +7,29 @@ require 'bandit/player/mpc'
 module Bandit
   class Main
     def self.run(argv)
-      # if we're not exploring, we'll try and choose a best album
-      album = Scores.best unless Config.exploring?
-
-      # if we're exploring, we don't have a best album, or we happen to
-      # choose the same album as last time, we'll choose one at random.
-      # TODO: if your player can't give us a list of albums this goes
-      # into an infinite loop...
-      while album.nil? || album == Store.last_album
+      # if we're explicitly exploring, or we don't have enough info to
+      # determine a best, then choose an album at random.
+      if Config.exploring? || (album = Scores.best).nil?
         album = Config.player.albums.sample
       end
 
-      # adjust the score for the current song
-      if (last_skip = Store.last_skip) && (current = Config.player.current)
+      raise "unable to find any albums." unless album
+
+      if album == (current = Config.player.current)
+        # same as current, just try again
+        return run(argv)
+      end
+
+      if current && (last_skip = Store.last_skip)
         Scores.adjust(current, Time.now - last_skip)
       end
 
-      # load the chosen album
       Config.player.load(album)
 
-      # store some info for next time
-      Store.last_skip  = Time.now
-      Store.last_album = album
-      Store.save
-
       puts %{ => "#{album}" loaded. }
+
+      Store.last_skip = Time.now
+      Store.save
 
     rescue => ex
       $stderr.puts "error: #{ex}"
