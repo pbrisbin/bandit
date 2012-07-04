09fa8972b25bc223f4fb5a8e69a74b55d65b57c9
patrick brisbin <pbrisbin@gmail.com>
2012-07-03 22:32:59 -0400

    finalize main method

--- lib/bandit.rb
+++ lib/bandit.rb
@@ -1,29 +1,42 @@
 require 'bandit/config'
 require 'bandit/scores'
 require 'bandit/store'
+require 'bandit/player'
+require 'bandit/player/mpc'
 
 module Bandit
   class Main
     def self.run(argv)
-      player = Config.player
-      album  = nil
+      # if we're not exploring, we'll try and choose a best album
+      album = Scores.best unless Config.exploring?
 
-      while !album || album == Store.last_album
-        if Config.exploring?
-          album = player.albums.sample
-        else
-          album = Scores.best || player.albums.sample
-        end
+      # if we're exploring, we don't have a best album, or we happen to
+      # choose the same album as last time, we'll choose one at random.
+      # TODO: if your player can't give us a list of albums this goes
+      # into an infinite loop...
+      while album.nil? || album == Store.last_album
+        album = Config.player.albums.sample
       end
 
-      if last_skip = Store.last_skip
-        Scores.adjust(player.current, Time.now - last_skip)
+      # adjust the score for the current song
+      if (last_skip = Store.last_skip) && (current = Config.player.current)
+        Scores.adjust(current, Time.now - last_skip)
       end
 
-      player.load(album)
+      # load the chosen album
+      Config.player.load(album)
 
+      # store some info for next time
       Store.last_skip  = Time.now
       Store.last_album = album
+      Store.save
+
+      puts %{ => "#{album}" loaded. }
+
+    rescue => ex
+      $stderr.puts "error: #{ex}"
+      $stderr.puts "#{ex.backtrace.join("\n")}"
+      exit 1
     end
   end
 end
