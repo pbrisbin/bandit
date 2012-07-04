49818644355c0e9e3e42788901ef55603acb4485
patrick brisbin <pbrisbin@gmail.com>
2012-07-03 21:31:04 -0400

    add a storage context

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,4 +29,16 @@ module Bandit
       Config.stub(:player).and_return(player)
     end
   end
+
+  shared_context 'tmp-storage' do
+    let(:tmp_file) { '/tmp/bandit_testing.pstore' }
+
+    before do
+      Config.stub(:storage).and_return(tmp_file)
+    end
+
+    after do
+      File.unlink(tmp_file) if File.exists?(tmp_file)
+    end
+  end
 end
